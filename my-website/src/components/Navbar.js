5e9a10c9267678fc1019f259b449476fafc4290a
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2020-12-22 00:47:04 +0800

    added temporary menu in nav

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, Component } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { menuData } from '../data/MenuData';
 
 // class Navbar extends Component {
 
@@ -37,14 +38,21 @@ const MenuBars = styled.i``;
 
 const NavMenu = styled.div``;
 
-
+const NavMenuLinks = styled(Link)`
+    colour: #fff; 
+`;
 function Navbar() {
     return (
         <Nav>
-            <Logo>Jeevan </Logo>
+            <Logo to="/"> Jeevan </Logo>
             <MenuBars />
             <NavMenu>
-
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+                    
             </NavMenu>
         </Nav>
     )
