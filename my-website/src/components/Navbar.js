f2005ba8e0b35961fc04a5ab9ced88da1647242c
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2020-12-22 13:57:59 +0800

    added a button

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React, { useState, Component } from 'react'
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components/macro';
 import { menuData } from '../data/MenuData';
+import { Button } from './Button';
 
 // class Navbar extends Component {
 
@@ -27,20 +28,48 @@ import { menuData } from '../data/MenuData';
 
 const Nav = styled.nav`
     height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
     background: red;
 `;
 
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
 const Logo = styled(Link)`
-    colour: #fff;
+    ${NavLink}
+    font-style: italic;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
+`;
 
 const NavMenuLinks = styled(Link)`
-    colour: #fff; 
+    ${NavLink}; 
 `;
+
+const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+`;
+
 function Navbar() {
     return (
         <Nav>
@@ -52,11 +81,16 @@ function Navbar() {
                         {item.title}
                     </NavMenuLinks>
                 ))}
-                    
             </NavMenu>
+            <NavBtn>
+                <Button to='/contact' primary='true'> Contact Me </Button>
+            </NavBtn>
         </Nav>
     )
 }
+
+
+// before i watched the video 
 
 function previousNavbar() {
     return (
