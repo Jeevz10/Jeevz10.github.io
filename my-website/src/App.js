597af6062d18606710810855204c587a57065f93
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2020-12-29 00:04:28 +0800

    added in other styled nav bar components

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
--- my-website/src/components/Navbar/index.js
+++ my-website/src/components/Navbar/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>
-
-                    </NavLogo>
+                    <NavLogo to='/'> Dolla </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
