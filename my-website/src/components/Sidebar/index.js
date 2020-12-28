98fa469385ba255b0c50ba2647d394a306eacbd6
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2020-12-29 01:08:12 +0800

    sidebar menu for mobile

---
+++ my-website/src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SideBarContainer, Icon, CloseIcon } from './SideBarElements';
+
+const index = () => {
+    return (
+        <SideBarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SideBarContainer>
+    )
+}
+
+export default index
