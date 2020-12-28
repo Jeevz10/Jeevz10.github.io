724bf202eea59c2d273cb1e0f2693b210266e79f
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2020-12-29 00:57:20 +0800

    polished the top

--- my-website/src/components/Navbar/index.js
+++ my-website/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+// hamburger menu, check out react-icons 
 import { FaBars } from 'react-icons/fa';
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
 
 const Navbar = () => {
     return (
@@ -12,10 +13,28 @@ const Navbar = () => {
                         <FaBars />
                     </MobileIcon>
                     <NavMenu>
+
                         <NavItem>
-                            <NavLinks to='about'>About</NavLinks>
+                            <NavLinks to='Profile'>Profile</NavLinks>
                         </NavItem>
+
+                        <NavItem>
+                            <NavLinks to='Skillsets'>Skillsets</NavLinks>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLinks to='Work Experience'>Work Experience</NavLinks>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLinks to='Projects'>Projects</NavLinks>
+                        </NavItem>
+
                     </NavMenu>
+
+                    <NavBtn>
+                        <NavBtnLink to='/signin'> Sign In </NavBtnLink>
+                    </NavBtn>
                 </NavbarContainer>
             </Nav>
         </>
