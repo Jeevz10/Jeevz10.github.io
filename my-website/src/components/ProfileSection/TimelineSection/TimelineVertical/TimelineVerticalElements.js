a4105005ac2f8256d8ecafba17de1af5a77b1fce
Jeevz10 <theghosthaunter@hotmail.com>
2021-01-14 10:56:46 +0800

    added a lil height

--- my-website/src/components/ProfileSection/TimelineSection/TimelineVertical/TimelineVerticalElements.js
+++ my-website/src/components/ProfileSection/TimelineSection/TimelineVertical/TimelineVerticalElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
 
 export const TimelineSection = styled.div`
-    height: 800px;
+    height: 850px;
     width: 1200px;
     margin: auto;
     background: #f2f2f2;
